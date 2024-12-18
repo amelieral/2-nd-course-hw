ed2fae6c8d7f3e0c3d8aea1c841ec16b1dc15aee
ameleiral <meilawther4@gmail.com>
2024-12-18 18:21:52 +0300

    Added file js files

--- JS/game1.js
+++ JS/game1.js
@@ -1,15 +1,34 @@
+const ALERT_MESSAGE_DEFAULT = 'Отгадай число от 1 до 100';
+
 function game1 (){
     const a = Math.floor(Math.random() *100) +1;
-    let trueNum = 0
+    let userInput = prompt(`Отгадай число от 1 до 100`);
+    let trueNum = Number(userInput);
 
-    do {
-        let trueNum = Number (prompt(`Отгадай число от 1 до 100`));
-        if (trueNum === a) {
-            alert(`Угадал`);
-        } else if (trueNum > a) {
-            alert(`Загаданное число меньше. Попробуй снова!`);
-        } else if (trueNum < a) {
-            alert(`Загаданное число больше. Попробуй снова!`);
-        }
-    } while (trueNum !== a);
+    if (!isNaN(trueNum) && userInput != null) {
+        do {
+            /** trueNum is null значит пользователь ничего не ввел */
+            if (trueNum !== null) {
+                console.log(trueNum)
+
+                if (trueNum === a) {
+                    alert(`Угадал`);
+                } else if (isNaN(trueNum) && trueNum !== null) {
+                    alert(`Вы ввели строку!`);
+                    trueNum = prompt(ALERT_MESSAGE_DEFAULT);
+                } else if (trueNum > a) {
+                    alert(`Загаданное число меньше. Попробуй снова!`);
+                    trueNum = prompt(ALERT_MESSAGE_DEFAULT);
+                } else if (trueNum < a) {
+                    alert(`Загаданное число больше. Попробуй снова!`);
+                    trueNum = prompt(ALERT_MESSAGE_DEFAULT);
+                }
+            }
+            else {
+                break;
+            }
+        } while (trueNum !== a);
+    } else if (userInput != null) {
+        alert(`Не честно, вы ввели строку, начните заного!`);
+    }
 }
