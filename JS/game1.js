454d21df4ccffd7f71a7387d484221a902ff26b0
ameleiral <meilawther4@gmail.com>
2025-01-06 12:33:21 +0300

    fixed file game1.js

--- JS/game1.js
+++ JS/game1.js
@@ -1,34 +1,26 @@
-const ALERT_MESSAGE_DEFAULT = 'Отгадай число от 1 до 100';
+function game1() {
+    const a = Math.floor(Math.random() * 100) + 1;
 
-function game1 (){
-    const a = Math.floor(Math.random() *100) +1;
-    let userInput = prompt(`Отгадай число от 1 до 100`);
-    let trueNum = Number(userInput);
+    while (true) {
+        let userInput = prompt('Отгадай число от 1 до 100');
 
-    if (!isNaN(trueNum) && userInput != null) {
-        do {
-            /** trueNum is null значит пользователь ничего не ввел в поле*/
-            if (trueNum !== null) {
-                console.log(trueNum)
 
-                if (trueNum === a) {
-                    alert(`Угадал`);
-                } else if (isNaN(trueNum) && trueNum !== null) {
-                    alert(`Вы ввели строку!`);
-                    trueNum = prompt(ALERT_MESSAGE_DEFAULT);
-                } else if (trueNum > a) {
-                    alert(`Загаданное число меньше. Попробуй снова!`);
-                    trueNum = prompt(ALERT_MESSAGE_DEFAULT);
-                } else if (trueNum < a) {
-                    alert(`Загаданное число больше. Попробуй снова!`);
-                    trueNum = prompt(ALERT_MESSAGE_DEFAULT);
-                }
-            }
-            else {
-                break;
-            }
-        } while (trueNum !== a);
-    } else if (userInput != null) {
-        alert(`Не честно, вы ввели строку, начните заного!`);
+        if (userInput === null) {
+            alert('Игра завершена.');
+            break;
+        }
+
+        let trueNum = Number(userInput);
+
+        if (isNaN(trueNum)) {
+            alert('Вы ввели строку! Пожалуйста, введите число.');
+        } else if (trueNum === a) {
+            alert(`Угадал! Загаданное число: ${a}`);
+            break;
+        } else if (trueNum > a) {
+            alert('Загаданное число меньше. Попробуй снова!');
+        } else if (trueNum < a) {
+            alert('Загаданное число больше. Попробуй снова!');
+        }
     }
 }
